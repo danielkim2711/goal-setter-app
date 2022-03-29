e8698af5219da247ee66c6d33bc4bdd18f13db3e
Daniel Kim <danielkim2711@gmail.com>
2022-03-29 22:13:23 +1300

    Add routes for goals

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,7 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 
+// Routes
+app.use('/api/goals', require('./routes/goalRoutes'));
+
 app.listen(port, () => console.log(`Server started on PORT ${port}`));
