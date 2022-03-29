9f8281ef7cd02cf8a0d863f334566ae500a2c514
Daniel Kim <danielkim2711@gmail.com>
2022-03-29 21:35:55 +1300

    Add express server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
-console.log('Hello World');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.listen(port, () => console.log(`Server started on PORT ${port}`));
