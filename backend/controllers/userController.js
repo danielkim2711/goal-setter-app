647e4278bef2eefcbbdfd74651ee9ecaf1d3975e
Daniel Kim <danielkim2711@gmail.com>
2022-04-04 23:10:33 +1200

    Add register user functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,51 @@
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/userModel');
+
 // @desc    Register a new user
 // @route   POST /api/users
 // @access  Public
-const registerUser = (req, res) => {
-  res.json({ message: 'Register User' });
-};
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please add all fields');
+  }
+
+  // Find if user already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
 
 // @desc    Authenticate a user
 // @route   POST /api/users/login
@@ -17,6 +59,13 @@ const loginUser = (req, res) => {
 // @access  Private
 const getMe = (req, res) => {
   res.json({ message: 'Get User' });
+};
+
+// Generate token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
 };
 
 module.exports = { registerUser, loginUser, getMe };
