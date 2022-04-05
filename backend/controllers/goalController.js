c733654a21628df020ad8018f78291b6bc835b43
Daniel Kim <danielkim2711@gmail.com>
2022-04-05 23:01:21 +1200

    Get logged in user's goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -2,11 +2,11 @@ const asyncHandler = require('express-async-handler');
 
 const Goal = require('../models/goalModel');
 
-// @desc    Get goals
+// @desc    Get user's goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find();
+  const goals = await Goal.find({ user: req.user._id });
 
   res.status(200).json(goals);
 });
