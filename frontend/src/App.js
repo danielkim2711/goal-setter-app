d161299031e41b663de19f66c0e5f819b35be13f
Daniel Kim <danielkim2711@gmail.com>
2022-04-06 16:30:52 +1200

    Create header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Header from './components/Header';
+
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -9,6 +11,7 @@ function App() {
   return (
     <Router>
       <div class='container'>
+        <Header />
         <Routes>
           <Route path='/' element={<Dashboard />} />
           <Route path='/login' element={<Login />} />
