cd30696b03bbf06d0d1106bb212a068d6b51545d
Daniel Kim <danielkim2711@gmail.com>
2022-04-06 16:11:08 +1200

    Create and set routes for pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import NotFound from './pages/NotFound';
+
 function App() {
   return (
-    <div>
-      <h1>My App</h1>
-    </div>
+    <Router>
+      <div class='container'>
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/*' element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
