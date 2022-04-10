ee956a67d1533b1c8075b282843f3200ddaec409
Daniel Kim <danielkim2711@gmail.com>
2022-04-10 22:49:39 +1200

    Get goals at dashboard page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,18 +1,38 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { getGoals, reset } from '../features/goals/goalSlice';
 import GoalForm from '../components/GoalForm';
+import Spinner from '../components/Spinner';
 
 const Dashboard = () => {
   const { user } = useSelector((state) => state.auth);
+  const { goals, isError, isLoading, message } = useSelector(
+    (state) => state.goals
+  );
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   useEffect(() => {
+    if (isError) {
+      console.log(message);
+    }
+
     if (!user) {
       navigate('/login');
     }
-  }, [user, navigate]);
+
+    dispatch(getGoals());
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, isError, message, navigate, dispatch]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
