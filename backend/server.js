de3dc190c805b943a267bcb4b57aca164d2c58f1
Daniel Kim <danielkim2711@gmail.com>
2022-04-13 19:38:55 +1200

    Deploy on heroku

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
@@ -15,6 +16,20 @@ app.use(express.urlencoded({ extended: false }));
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/goals', require('./routes/goalRoutes'));
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  );
+} else {
+  app.get('/', (req, res) =>
+    res.status(200).json({ message: 'Welcome to the Goal Setter App API' })
+  );
+}
 
 app.use(errorHandler);
 
